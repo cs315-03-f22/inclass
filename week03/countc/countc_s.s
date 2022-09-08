4ef1c565fd38e8030c8f579af76d9c6c18551fbc
Phil Peterson <phpeterson@usfca.edu>
2022-09-08 18:08:50 +0000

    readability

--- week03/countc/countc_s.s
+++ week03/countc/countc_s.s
@@ -1,4 +1,5 @@
 .global countc_s
+.global findc_s
 
 /* findc_s returns true if c exists in s 
 	else false
@@ -15,26 +16,27 @@ findc_s:
 	a1 = char
 */
 countc_s:
-					# prologue
-	addi sp, sp, -16	# alloc stack space
-	sd ra, 0(sp)	# preserve ra
+								# prologue
+	addi sp, sp, -16			# alloc stack space
+	sd ra, 0(sp)				# preserve ra
 	
-	li t0, 0		# init count
+	li t0, 0					# init count
 loop:
-	lb t1, (a0)		# t1 = *a0
+	lb t1, (a0)					# t1 = *a0
 	beq t1, zero, done_count	# reached null terminator?
 
-	sd a0, 8(sp)	# preserve a0
+	sd a0, 8(sp)				# preserve a0
+	# li a0, 0
 	jal findc_s	
-	beq a0, zero, count_next
-	addi t0, t0, 1 	# count++
-count_next:
-	ld a0, 8(sp)	# restore a0
-	addi a0, a0, 1 	# s++
+	beq a0, zero, next			# not found, next
+	addi t0, t0, 1 				# found, count++
+next:
+	ld a0, 8(sp)				# restore a0
+	addi a0, a0, 1 				# s++
 	j loop
 done_count:
-	mv a0, t0		# set up count as retval
+	mv a0, t0					# set up count as retval
 
-					# epilogue
-	ld ra, 0(sp)	# restore ra
+								# epilogue
+	ld ra, 0(sp)				# restore ra
 	ret
