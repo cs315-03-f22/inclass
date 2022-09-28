7f8540674e0a0110f20712b63605ee74f1ae2920
Phil Peterson <phpeterson@usfca.edu>
2022-09-28 23:33:23 +0000

    update 9/28

--- week06/emu/rv_emu.c
+++ week06/emu/rv_emu.c
@@ -16,15 +16,23 @@ void rv_init(struct rv_state *state, uint32_t *func,
     state->pc = (uint64_t) func;
 }
 
+// Phil's approach: helper function for shift-and-mask
+uint32_t get_bits(uint32_t iw, uint32_t start, uint32_t count) {
+    uint32_t mask = (1 << count) - 1;
+    return (iw >> start) & mask;
+}
+
 void emu_r_type(struct rv_state *state, uint32_t iw) {
-    uint32_t rd = (iw >> 7) & 0b11111;
+    uint32_t rd = get_bits(iw, 7, 5); //(iw >> 7) & 0b11111;
     uint32_t rs1 = (iw >> 15) & 0b11111;
     uint32_t rs2 = (iw >> 20) & 0b11111;
     uint32_t func3 = (iw >> 12) & 0b111;
+    uint32_t func7 = (iw >> 25) & 0x7F;
 
     switch (func3) {
-        case 0b000:  // add
-            state->regs[rd] = state->regs[rs1] + state->regs[rs2];
+        case 0b000:
+            if (func7 == 0) //add
+                state->regs[rd] = state->regs[rs1] + state->regs[rs2];
             break;
         default:
             printf("unsupported rtype func3\n");
@@ -33,12 +41,29 @@ void emu_r_type(struct rv_state *state, uint32_t iw) {
     state->pc += 4;
 }
 
+void emu_jalr(struct rv_state *state, uint32_t iw) {
+    uint32_t rs1 = (iw >> 15) & 0x1f; // will be 1/ra
+
+    state->pc = state->regs[rs1];  // pc = ra
+}
+
+// Greg's approach: make up a decode struct which uses the
+// layout of RISC-V machine code to decode iw once
+struct rv_decode {
+    uint32_t rd;
+    uint32_t rs1;
+    //... 
+};
+
 void rv_one(struct rv_state *state, uint32_t iw) {
     uint32_t opcode = iw & 0b1111111;
 
     switch(opcode) {
         case 0b0110011:  // R-type instruction
             emu_r_type(state, iw);
+            break;
+        case 0b1100111:  // jalr
+            emu_jalr(state, iw);
             break;
         default:
             printf("unsupported opcode\n");
